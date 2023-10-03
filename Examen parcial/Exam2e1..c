67cb045ce8482b9a6f4e9e05f2aebcf255db8ee5
The-Dotcom-code <142333668+The-Dotcom-code@users.noreply.github.com>
2023-10-03 15:53:53 -0600

    Update Exam2e1..c
    Divisiones

--- Examen parcial/Exam2e1..c
+++ Examen parcial/Exam2e1..c
@@ -6,7 +6,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	int i,j,x, m,n, r;
+	int i,j,x;
+	float m,n, r;
 	srand(time(NULL));
 	x=rand() % (5) +1;
 	
@@ -16,25 +17,25 @@ int main() {
 				m=rand() % (9) +1;
 				n=rand() % (9) +1;
 				r= m + n;
-				printf("%d + %d = %d\n", m,n,r);	
+				printf("%.2f + %.2f = %.2f\n", m,n,r);	
 				break;
 			case 2:
 				m=rand() % (9) +1;
 				n=rand() % (9) +1;
 				r= m - n;
-				printf("%d - %d = %d\n", m,n,r);	
+				printf("%.2f - %.2f = %.2f\n", m,n,r);	
 				break;	
 			case 3:
 				m=rand() % (9) +1;
 				n=rand() % (9) +1;
 				r= m * n;
-				printf("%d * %d = %d\n", m,n,r);	
+				printf("%.2f * %.2f = %.2f\n", m,n,r);	
 				break;
 			case 4:
 				m=rand() % (9) +1;
 				n=rand() % (9) +1;
 				r= m / n;
-				printf("%d / %d = %d\n", m,n,r);	
+				printf("%.2f / %.2f = %.2f\n", m,n,r);	
 				break;
 		}
 		x=rand() % (5) +1;
